ea72199a4a8877e52c2ca7d889e979062bcd0c79
omama zainab <omamazainab00@gmail.com>
2021-07-28 18:31:48 +0500

    Updates

---
+++ precache-manifest.09702f51ab93123aaf836c093c2b1b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24fcd65780aff44298e4a512cf2eb57",
+    "url": "/Random-quote-generator/index.html"
+  },
+  {
+    "revision": "09da9eccfc9808081878",
+    "url": "/Random-quote-generator/static/css/main.4cb9770c.chunk.css"
+  },
+  {
+    "revision": "a6905e0149209f8cd93d",
+    "url": "/Random-quote-generator/static/js/2.d098ceca.chunk.js"
+  },
+  {
+    "revision": "09da9eccfc9808081878",
+    "url": "/Random-quote-generator/static/js/main.5ac6f74e.chunk.js"
+  },
+  {
+    "revision": "d5c8365b5324f07416bd",
+    "url": "/Random-quote-generator/static/js/runtime-main.42e18b5c.js"
+  }
+]);
